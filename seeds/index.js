826bbd87ce1bc35add95e4bd1f93b615e07a38f3
Sarah Beth Locklar <110067009+SBLOCKLAR@users.noreply.github.com>
2023-05-16 21:48:43 -0400

    Ignore ENV

--- seeds/index.js
+++ seeds/index.js
@@ -22,19 +22,26 @@ const seedDB = async () => {
       author: "645913e11e34cff9c67624ec",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
-      images: [
-        {
-          url: "https://res.cloudinary.com/dq8illray/image/upload/v1683858203/YelpCamp/wdhqzk277pbiz7bc576g.png",
-          filename: "YelpCamp/wdhqzk277pbiz7bc576g",
-        },
-        {
-          url: "https://res.cloudinary.com/dq8illray/image/upload/v1683858208/YelpCamp/ixqppdkjhxlsw8vjmiip.jpg",
-          filename: "YelpCamp/ixqppdkjhxlsw8vjmiip",
-        },
-      ],
       description:
         "Meow and walk away take a deep sniff of sock then walk around with mouth half open use lap as chair stare out the window, so run outside as soon as door open ignore the human until she needs to get up, then climb on her lap and sprawl. You are a captive audience while sitting on the toilet, pet me wake up wander around the house making large amounts of noise",
       price,
+      geometry: {
+        type: "Point",
+        coordinates: [
+          cities[random1000].longitude,
+          cities[random1000].latitude,
+        ],
+      },
+      images: [
+        {
+          url: "https://res.cloudinary.com/dq8illray/image/upload/v1683838912/YelpCamp/djs37phjcbiz7mya0vfy.jpg",
+          filename: "YelpCamp/djs37phjcbiz7mya0vfy",
+        },
+        {
+          url: "https://res.cloudinary.com/dq8illray/image/upload/v1683855575/YelpCamp/zsrmymwkaxfzypuyxdtn.jpg",
+          filename: "YelpCamp/zsrmymwkaxfzypuyxdtn",
+        },
+      ],
     });
     await camp.save();
   }
